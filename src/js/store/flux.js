2ce13b9923a0a260e32252c7ee0e7c0762b84e70
SilverFran <francisco1627@gmail.com>
2020-04-10 11:55:13 -0400

    making flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,24 @@
+const url = "https://assets.breatheco.de/apis/fake/contact/";
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			loadContact() {
+				fetch(url + "agenda/silver_agenda")
+					.then(response => response.json())
+					.then(result => {
+						console.log("Get Contact", result),
+							setStore({
+								contacts: result
+							});
+					})
+					.catch(e => console.error(e));
+			}
 		}
 	};
 };
